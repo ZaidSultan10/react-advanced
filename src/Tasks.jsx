d137b05947afcf1834873a5fdcd8231a6e35655f
zaidsultan10 <zaidsultan09@gmail.com>
2023-06-27 01:26:49 +0400

    todo list with states and local storage

--- src/Tasks.jsx
+++ src/Tasks.jsx
@@ -1,9 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 const Tasks = () => {
+    const LOCAL_STORAGE_KEY = "LOCAL_STORAGE_KEY"
+    const storeInLocalStorage = (taskStorage) => localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(taskStorage))
+    const readAllItems = () => {
+        let readItems = JSON.parse(localStorage.getItem('LOCAL_STORAGE_KEY'))
+        return readItems && Object.keys(readItems).length > 0 ? readItems : {tasks:[], completedTasks:[]}
+    }
+    const getStorage = readAllItems()
     const [taskText, setTaskText] = useState('')
-    const [tasks, setTasks] = useState([])
-    const [completedTasks, setCompletedTasks] = useState([])
+    const [tasks, setTasks] = useState(getStorage.tasks)
+    const [completedTasks, setCompletedTasks] = useState(getStorage.completedTasks)
+
+    useEffect(() => {
+        storeInLocalStorage({tasks, completedTasks})
+    }, [])
 
     const handleChange = (e) => {
         setTaskText(e.target.value)
