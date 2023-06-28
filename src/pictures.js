ee1e0fc2ddd8a859ba1bb6fd17c8c38c4bb842bd
zaidsultan10 <zaidsultan09@gmail.com>
2023-06-28 17:22:25 +0400

    picture assets for gallery

---
+++ src/pictures.js
@@ -0,0 +1,22 @@
+import PICTURE_1 from './hooks-assets/bridge.jpeg'
+import PICTURE_2 from './hooks-assets/lake.jpeg'
+import PICTURE_3 from './hooks-assets/lion.jpeg'
+import PICTURE_4 from './hooks-assets/peach.jpeg'
+import PICTURE_5 from './hooks-assets/waterfall.jpeg'
+
+export const PICTURES = [{
+    id:0,
+    image:PICTURE_1
+},{
+    id:1,
+    image:PICTURE_2
+},{
+    id:2,
+    image:PICTURE_3
+},{
+    id:3,
+    image:PICTURE_4
+},{
+    id:4,
+    image:PICTURE_5
+}]
